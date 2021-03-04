3d18303f6e2ff6022beffad0228d527796e2c0f0
Anthony Costabile <anthony@tallyit.com.au>
2021-03-05 05:49:13 +1100

    fix: time properly displayed as seconds

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,7 +1,9 @@
 import { CancelMessage, TrackedError } from './types';
 
 const defaultCancelMessage: CancelMessage = ({ action, time }) =>
-  `action ${action} cancelled after ${new Date().valueOf() - time} seconds`;
+  `action ${action} cancelled after ${
+    (new Date().valueOf() - time) / 1000
+  } seconds`;
 
 const defaultError = () => new Error('Unknown API error occured');
 
